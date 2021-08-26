c7d4037f5f23a8975fc646538a68b85fd13814b8
GeetanjaliTest <giitanjalim@gmail.com>
2021-08-26 23:15:34 +0530

    Create kp2.js

---
+++ kp2.js
@@ -0,0 +1,24 @@
+if(obstaclesGroup.isTouching(kangaroo)){
+      collidedSound.play();
+      gameState = END;
+    }
+    if(shrubsGroup.isTouching(kangaroo)){
+
+      shrubsGroup.destroyEach();
+    }
+  }
+  else if (gameState === END) {
+    //set velcity of each game object to 0
+    kangaroo.velocityY = 0;
+    jungle.velocityX = 0;
+    obstaclesGroup.setVelocityXEach(0);
+    shrubsGroup.setVelocityXEach(0);
+
+    //change the trex animation
+    kangaroo.changeAnimation("collided",kangaroo_collided);
+    
+    //set lifetime of the game objects so that they are never destroyed
+    obstaclesGroup.setLifetimeEach(-1);
+    shrubsGroup.setLifetimeEach(-1);
+    
+  }
